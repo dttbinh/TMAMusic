414ef0117590780cbfb069ea7ab7e1556d6ba618
dttbinh <dttbinh@tma.com.vn>
2018-01-17 22:16:20 +0700

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes,ActivatedRoute} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 
+import { HomePageComponent } from './homepage/homepage.component';
+import { HeaderPageComponent } from './homepage/headerpage/headerpage.component';
+import { ContentPageComponent } from './homepage/contentpage/contentpage.component';
+import { FooterPageComponent } from './homepage/footerpage/footerpage.component';
+
+  const routes:Routes=[
+     
+      {
+       path:'',
+       component: HomePageComponent
+     },
+      { path: '',
+        redirectTo: '/homepage',
+        pathMatch: 'full'
+        }
+
+   ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderPageComponent,
+    ContentPageComponent,
+    FooterPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/headerpage/headerpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headerpage',
+  templateUrl: './headerpage.component.html',
+  styleUrls: ['./headerpage.component.css']
+})
+export class HeaderPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
